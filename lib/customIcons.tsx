674f83a6d83330774d1800e1abec82a66b96d88c
TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>
2026-02-07 16:18:54 +0100

    Fixed closing tags and change img to Image

--- lib/customIcons.tsx
+++ lib/customIcons.tsx
@@ -1,24 +1,32 @@
-import { loader } from 'fumadocs-core/source';
-import { icons } from 'lucide-react';
-import { createElement, ReactNode } from 'react';
-import * as LMixer from "content/icons/lmixer.png"
+import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement, ReactNode } from "react";
+import * as LMixer from "content/icons/lmixer.png";
+import Image from "next/image";
 
 const customIcons = {
-  "LMixer": LMixer
-}
+  LMixer: LMixer,
+};
 
-export const iconResolver = function(icon: string | undefined): ReactNode {
-    if (!icon) {
-      return;
-    }
-
-    const iconSize = 24;
-
-    if (icon in customIcons) {
-      return <img width={iconSize} height={iconSize} src={customIcons[icon as keyof typeof customIcons].default.src}></img>
-    }
-
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-
+export const iconResolver = function (icon: string | undefined): ReactNode {
+  if (!icon) {
     return;
-  };
+  }
+
+  const iconSize = 24;
+
+  if (icon in customIcons) {
+    return (
+      <Image
+        width={iconSize}
+        height={iconSize}
+        src={customIcons[icon as keyof typeof customIcons].default.src}
+        alt=""
+      ></Image>
+    );
+  }
+
+  if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+
+  return;
+};
