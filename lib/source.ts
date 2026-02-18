a37a84c520c3c2859dbcf26a6868d222d77852e7
TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>
2026-02-18 14:50:54 +0100

    Missing semicolon
    Co-authored-by: Mattias Mats Mathew Matte Orahem <99484356+Cocowhal@users.noreply.github.com>

--- lib/source.ts
+++ lib/source.ts
@@ -1,7 +1,7 @@
 import { docs } from 'fumadocs-mdx:collections/server';
 import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
-import { iconResolver } from "./custom-icons"
+import { iconResolver } from "./custom-icons";
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
