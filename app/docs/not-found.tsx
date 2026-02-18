2887361f71fbd054302b62da31a364218698fb32
TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>
2026-02-18 10:42:57 +0100

    Change <XYZ></XYZ> to <XYZ/>

--- app/docs/not-found.tsx
+++ app/docs/not-found.tsx
@@ -1,5 +1,5 @@
 import NotFoundComponent from "../../components/generic/notfound/not-found";
 
 export default function NotFound() {
-  return <NotFoundComponent></NotFoundComponent>;
+  return <NotFoundComponent/>;
 }
