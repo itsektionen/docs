e36c14bea167c4c046f6a7a151eee7d63002fb9c
epicmeme <simonculus@gmail.com>
2026-02-19 14:49:03 +0100

    i love lowercase, fight me

--- lib/pathname-mapper.ts
+++ lib/pathname-mapper.ts
@@ -1,56 +1,52 @@
-
-
 export enum DocLocations {
-    Default,
-    Kistan,
-    Init,
+  Default,
+  Kistan,
+  Init,
 }
 
 const docMappings: { matchString: string; location: DocLocations }[] = [
-    { matchString: "kistan", location: DocLocations.Kistan },
-    { matchString: "heim", location: DocLocations.Init },
-    { matchString: "slinky", location: DocLocations.Init },
+  { matchString: "kistan", location: DocLocations.Kistan },
+  { matchString: "heim", location: DocLocations.Init },
+  { matchString: "slinky", location: DocLocations.Init },
 ];
 
 export type LocationInfo = {
-    title: string;
-    notFoundImage?: {
-        imageName: string,
-        imageAlt: string
-    };
-}
-
-const locationInfos: {
-    [key in DocLocations]: LocationInfo;
-} = {
-    [DocLocations.Default]: {
-        title: "IT/docs",
-    },
-    [DocLocations.Kistan]: {
-        title: "Kistan/docs",
-        notFoundImage: {
-            imageName: "SiMoN",
-            imageAlt: "SiMoN"
-        }
-    },
-    [DocLocations.Init]: {
-        title: "init/docs"
-    },
+  title: string;
+  notFoundImage?: {
+    imageName: string;
+    imageAlt: string;
+  };
 };
 
-
+const locationInfos: {
+  [key in DocLocations]: LocationInfo;
+} = {
+  [DocLocations.Default]: {
+    title: "it/docs",
+  },
+  [DocLocations.Kistan]: {
+    title: "kistan/docs",
+    notFoundImage: {
+      imageName: "SiMoN",
+      imageAlt: "SiMoN",
+    },
+  },
+  [DocLocations.Init]: {
+    title: "init/docs",
+  },
+};
 
 export function getMapping(pathName: string): DocLocations {
-    pathName = pathName + "/";
-    const mapping = docMappings.find((mapping) => {
-        return pathName.startsWith("/docs/" + mapping.matchString + "/");
-    });
-    if (mapping === undefined) {
-        return DocLocations.Default;
-    }
-    return mapping.location;
+  pathName = pathName + "/";
+  const mapping = docMappings.find((mapping) => {
+    return pathName.startsWith("/docs/" + mapping.matchString + "/");
+  });
+  if (mapping === undefined) {
+    return DocLocations.Default;
+  }
+  return mapping.location;
 }
 
 export function getLocationInfo(location: DocLocations): LocationInfo {
-    return locationInfos[location]
+  return locationInfos[location];
 }
