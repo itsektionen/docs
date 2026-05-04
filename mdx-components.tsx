de543f0e1871e706930542c019252e3b22ab4e00
TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>
2026-05-04 13:05:34 +0200

    Make LMixer Components non-global (#5)

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,5 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-import FixtureTypes from "./components/lmixer/fixtures/fixture-types";
-import FixtureInfos from "./components/lmixer/fixtures/fixture-infos";
-import FixtureGroups from "./components/lmixer/fixtures/fixture-groups";
-import LayerTreeExplaination from "./components/lmixer/layer/layer-tree-explaination";
-import DrawAllLayers from "./components/lmixer/layer/draw-all-layers";
 import ExclusiveTo from "./components/generic/callout/exclusive-to";
 import CalloutExt from "./components/generic/callout/callout-ext";
 import Icon from "./components/generic/icon";
@@ -14,11 +9,6 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
-    FixtureTypes: FixtureTypes,
-    FixtureInfos: FixtureInfos,
-    FixtureGroups: FixtureGroups,
-    LayerTreeExplaination: LayerTreeExplaination,
-    DrawAllLayers: DrawAllLayers,
     ExclusiveTo: ExclusiveTo,
     CalloutExt: CalloutExt,
     Icon: Icon,
