c34bce6a8aa47375ad8f3dee2a7b05fda4bda46b
TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>
2026-05-04 13:00:39 +0200

    Move not-found.tsx and add DocsLayout (#7)

--- components/generic/notfound/not-found.tsx
+++ components/generic/notfound/not-found.tsx
@@ -8,6 +8,8 @@ import { source } from "@/lib/source";
 import Card, { CardType } from "../card";
 import { DocsPage } from "fumadocs-ui/layouts/docs/page";
 import { Cards } from "fumadocs-ui/components/card";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { baseOptions } from "@/lib/layout.shared";
 
 export default function NotFound() {
   const pathname = usePathname();
@@ -32,44 +34,46 @@ export default function NotFound() {
   });
 
   return (
-    <DocsPage full={true}>
-      <div className="p-5 prose">
-        <h1
-          className="mx-auto w-fit my-0!"
-          style={{
-            fontSize: "5rem",
-          }}
-        >
-          404
-        </h1>
-        <p className="mx-auto w-fit">Not found</p>
-        <div className="w-50 mx-auto">
-          {image && (
-            <Image
-              src={image}
-              alt={locationInfo.notFoundImage?.imageAlt ?? ""}
-            />
-          )}
+    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+      <DocsPage full={true}>
+        <div className="p-5 prose">
+          <h1
+            className="mx-auto w-fit my-0!"
+            style={{
+              fontSize: "5rem",
+            }}
+          >
+            404
+          </h1>
+          <p className="mx-auto w-fit">Not found</p>
+          <div className="w-50 mx-auto">
+            {image && (
+              <Image
+                src={image}
+                alt={locationInfo.notFoundImage?.imageAlt ?? ""}
+              />
+            )}
+          </div>
+          <hr />
+          <div>
+            <Cards>
+              {tabs.map((x) => {
+                return (
+                  <Card
+                    key={x.url}
+                    href={x.url}
+                    icon={x.icon}
+                    title={x.title}
+                    type={CardType.iconSlash}
+                  >
+                    {x.description}
+                  </Card>
+                );
+              })}
+            </Cards>
+          </div>
         </div>
-        <hr />
-        <div>
-          <Cards>
-            {tabs.map((x) => {
-              return (
-                <Card
-                  key={x.url}
-                  href={x.url}
-                  icon={x.icon}
-                  title={x.title}
-                  type={CardType.iconSlash}
-                >
-                  {x.description}
-                </Card>
-              );
-            })}
-          </Cards>
-        </div>
-      </div>
-    </DocsPage>
+      </DocsPage>
+    </DocsLayout>
   );
 }
