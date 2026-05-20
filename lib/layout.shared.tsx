375dbaff290c8333463db25bf90ee99687945653
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-05-20 14:05:02 +0200

    Remove fun

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,10 +1,9 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import TitleComponent from "./title-component";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: <TitleComponent />,
+      title: "it/docs",
     },
   };
 }
