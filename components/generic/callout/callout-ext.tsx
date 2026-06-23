f4545395fd9cc8b6ce562380e930df2b2bd82739
Fripe <72686066+Fripe070@users.noreply.github.com>
2026-06-23 20:34:26 +0200

    Rewrite CalloutExt (#29)
    * Rewrite CalloutExt
    
    * Use our own selection of callout types, instead of only being a subset of fumadocs
    
    * Revert details -> info
    
    * info -> note
    
    * Small fix
    
    * Fix invalid type for callout
    
    ---------
    
    Co-authored-by: TheUsernameOrSmth <105546141+TheUsernameOrSmth@users.noreply.github.com>

--- components/generic/callout/callout-ext.tsx
+++ components/generic/callout/callout-ext.tsx
@@ -1,146 +1,93 @@
-import { Callout, CalloutDescription } from "fumadocs-ui/components/callout";
 import {
-  BookText,
-  CircleCheck,
-  CircleX,
-  Hourglass,
-  Info,
-  Lightbulb,
-  Milestone,
-  Siren,
-  Skull,
-  TriangleAlert,
-} from "lucide-react";
+  CalloutContainer,
+  CalloutDescription,
+  CalloutTitle,
+  type CalloutContainerProps,
+} from "fumadocs-ui/components/callout";
 import { ReactNode } from "react";
-import { cn } from "@/lib/cn";
-import styles from "./generic.callout.calloutExt.module.css";
+import {
+  BookIcon,
+  HourglassIcon,
+  InfoIcon,
+  LightbulbIcon,
+  SirenIcon,
+  SkullIcon,
+  TriangleAlertIcon,
+} from "lucide-react";
 
-/*
+type CalloutConfig = {
+  title: string;
+  icon: ReactNode;
+  color: string;
+};
+type CalloutMap<T extends string = string> = Record<T, CalloutConfig>;
 
-Modified version of Fumadocs' <Callout/> Component
+const colorVar = (key: string) => `var(--color-${key}, var(--color-fd-muted))`;
+// we use stroked callout icons, while normal fumadocs uses filled icons
+const iconClass = "size-5 -me-0.5 stroke-(--callout-color) text-fd-card";
 
-*/
-
-const iconClassFill = "size-5 -me-0.5 fill-(--callout-color) text-fd-card";
-const iconClassStroke = "size-5 -me-0.5 stroke-(--callout-color) text-fd-card";
-
-export type CalloutExtType =
-  | "info"
-  | "warn"
-  | "error"
-  | "success"
-  | "idea"
-  | "note"
-  | "tip"
-  | "danger"
-  | "important"
-  | "details"
-  | "deprecated"
-  | "caution";
-
-const calloutTypes: {
-  [key in CalloutExtType]: {
-    header: string;
-    icon: ReactNode;
-    className: string;
-  };
-} = {
-  info: {
-    header: "Info",
-    icon: <Info className={iconClassStroke}></Info>,
-    className: "calloutext-info",
-  },
-  warn: {
-    header: "Warning",
-    icon: <TriangleAlert className={iconClassFill}></TriangleAlert>,
-    className: "calloutext-warn",
-  },
-  error: {
-    header: "Error",
-    icon: <CircleX className={iconClassFill}></CircleX>,
-    className: "calloutext-error",
-  },
-  success: {
-    header: "Success",
-    icon: <CircleCheck className={iconClassFill}></CircleCheck>,
-    className: "calloutext-success",
-  },
-  idea: {
-    header: "Idea",
-    icon: <Lightbulb className={iconClassStroke}></Lightbulb>,
-    className: "calloutext-idea",
-  },
+const calloutTypes = {
   note: {
-    header: "Note",
-    icon: <Info className={iconClassStroke}></Info>,
-    className: "calloutext-note",
-  },
-  caution: {
-    header: "Caution",
-    icon: <TriangleAlert className={iconClassStroke}></TriangleAlert>,
-    className: "calloutext-warn",
+    title: "Note",
+    icon: <InfoIcon className={iconClass} />,
+    color: colorVar("fd-info"),
   },
   tip: {
-    header: "Tip",
-    icon: <Milestone className={iconClassStroke}></Milestone>,
-    className: "calloutext-tip",
-  },
-  danger: {
-    header: "Danger",
-    icon: <Skull className={iconClassStroke}></Skull>,
-    className: "calloutext-danger",
+    title: "Tip",
+    icon: <LightbulbIcon className={iconClass} />,
+    color: colorVar("fd-info"),
   },
   important: {
-    header: "Important",
-    icon: <Siren className={iconClassStroke}></Siren>,
-    className: "calloutext-important",
+    title: "Important",
+    icon: <SirenIcon className={iconClass} />,
+    color: colorVar("fd-idea"),
+  },
+  warning: {
+    title: "Warning",
+    icon: <TriangleAlertIcon className={iconClass} />,
+    color: colorVar("fd-warning"),
+  },
+  danger: {
+    title: "Danger",
+    icon: <SkullIcon className={iconClass} />,
+    color: colorVar("fd-error"),
   },
   details: {
-    header: "Details",
-    icon: <BookText className={iconClassStroke}></BookText>,
-    className: "calloutext-details",
+    title: "Details",
+    icon: <BookIcon className={iconClass} />,
+    color: colorVar("fd-muted-foreground"),
   },
   deprecated: {
-    header: "Deprecated",
-    icon: <Hourglass className={iconClassStroke}></Hourglass>,
-    className: "calloutext-deprecated",
+    title: "Deprecated",
+    icon: <HourglassIcon className={iconClass} />,
+    color: "#bba66d",
   },
-};
+} as const satisfies CalloutMap;
+export type CalloutExtType = keyof typeof calloutTypes;
 
 export default function CalloutExt({
   children,
-  type,
   title,
+  type: inputType = "note",
+  icon,
   ...props
-}: {
-  children: ReactNode;
-  title?: string;
-  type: CalloutExtType;
+}: Omit<CalloutContainerProps, "type" | "title"> & {
+  type?: CalloutExtType;
+  title?: ReactNode;
 }) {
-  const calloutType = calloutTypes[type];
-
-  if (calloutType == undefined) {
-    throw new Error("Invalid Callout Type: " + type);
-  }
+  const calloutType = calloutTypes[inputType];
+  if (!calloutType) throw new Error(`Invalid callout type: ${inputType}`);
   return (
-    <div
-      className={cn(
-        "flex gap-2 my-4 rounded-xl border bg-fd-card p-3 ps-1 text-sm text-fd-card-foreground shadow-md",
-        styles["calloutext-base-style"],
-        styles[calloutType.className]
-      )}
+    <CalloutContainer
+      icon={icon ?? calloutType.icon}
+      // prettier-ignore
+      style={{
+        "--callout-color": calloutType.color,
+      } as React.CSSProperties}
       {...props}
     >
-      <div role="none" className="w-0.5 bg-(--callout-color)/50 rounded-sm" />
-      <div className="flex flex-col gap-2 min-w-0 flex-1">
-        {calloutType.header && (
-          <div className="flex gap-2">
-            {calloutType.icon}
-            <p className="font-medium my-0!">{title ?? calloutType.header}</p>
-          </div>
-        )}
-        <CalloutDescription>{children}</CalloutDescription>
-      </div>
-    </div>
+      <CalloutTitle>{title ?? calloutType.title}</CalloutTitle>
+      <CalloutDescription>{children}</CalloutDescription>
+    </CalloutContainer>
   );
 }
