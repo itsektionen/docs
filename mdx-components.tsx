68a53b7052bbeea891877d41c5bb3693079fa5cf
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-08-16 19:55:13 +0200

    Polish traefik docs with auth methods in separate tabs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,15 @@
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-import ExclusiveTo from "./components/generic/callout/exclusive-to";
 import CalloutExt from "./components/generic/callout/callout-ext";
-import Icon from "./components/generic/icon";
+import ExclusiveTo from "./components/generic/callout/exclusive-to";
 import Card from "./components/generic/card";
+import Icon from "./components/generic/icon";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
     ...components,
     ExclusiveTo: ExclusiveTo,
     CalloutExt: CalloutExt,
